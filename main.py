b46bb1471f6807a3bd64eae25c86781dc6570766
mehow <michalgacka@gmail.com>
2017-03-01 18:11:54 -0500

    Add trackbar for Canny; Find similar contours on board and piece

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import auxcv as aux
 import piecematchersift as pms
 import drag_and_crop as dac
+import trackbar as tb
 
 CANVAS_PATH = './data/image.jpg'
 PIECE_PATH = './data/piece.jpg'
@@ -24,7 +25,8 @@ cropped_board = dac.crop_image(pm.board)
 
 piece_masked_background = aux.image_to_gray(piece_masked_background.copy())
 cropped_board = aux.image_to_gray(cropped_board.copy())
-piece_masked_background = cv2.GaussianBlur(piece_masked_background, (5,5), 0)
+piece_masked_background = cv2.GaussianBlur(piece_masked_background, (7,7), 0)
+piece_masked_background = cv2.GaussianBlur(piece_masked_background, (7,7), 2)
 
 fig = plt.figure()
 ax1 = fig.add_subplot(211)
@@ -36,7 +38,23 @@ plt.hist(piece_masked_background[np.nonzero(piece_masked_background)].ravel(), 2
 #aux.show_image("piece", piece_masked_background, False)
 #aux.show_image("board", cropped_board, True)
 
-edges = cv2.Canny(piece_masked_background,30,200)
-aux.show_image("edges", edges, True)
+
+#edges1 = tb.CannyTrackbar(piece_masked_background, "cannyTrackbar")
+edges1 = cv2.Canny(piece_masked_background,0,68)
+#edges2 = tb.CannyTrackbar(cropped_board, "cannyTrackbar")
+edges2 = cv2.Canny(cropped_board, 165, 100)
+
+_, contours, heirarchy = cv2.findContours(edges1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+aux.show_image("piece", cropped_piece, False)
+aux.show_image("edges1", edges1, False)
+aux.show_image("edges2", edges2, True)
+
+cnts = sorted(contours, key = cv2.contourArea, reverse = True)
+cv2.drawContours(edges1, cnts[1:], 0, (0,255,0), 3)
+aux.show_image("contours", edges1, True)
+
+edges2 = cv2.resize(edges2.copy(), edges1.shape, edges2, fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
+aux.show_image("edges2smaller", edges2, True, True)
 
 cv2.destroyAllWindows()
--- trackbar.py
+++ trackbar.py
@@ -25,3 +25,27 @@ def SimpleTrackbar(img, win_name):
 
     cv2.destroyAllWindows()
     return trackbar_pos
+
+def CannyTrackbar(img, win_name):
+    trackbar_name = win_name + "Trackbar"
+
+    cv2.namedWindow(win_name)
+    cv2.resizeWindow(win_name, 500,100)
+    cv2.createTrackbar("first", win_name, 0, 255, empty_function)
+    cv2.createTrackbar("second", win_name, 0, 255, empty_function)
+    cv2.createTrackbar("third", win_name, 0, 255, empty_function)
+
+    while True:
+        trackbar_pos1 = cv2.getTrackbarPos("first", win_name)
+        trackbar_pos2 = cv2.getTrackbarPos("second", win_name)
+        trackbar_pos3 = cv2.getTrackbarPos("third", win_name)
+        img_blurred = cv2.GaussianBlur(img.copy(), (7,7), 2)
+        canny = cv2.Canny(img_blurred, trackbar_pos1, trackbar_pos2)
+        cv2.imshow(win_name, canny)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("c"):
+            break
+
+    cv2.destroyAllWindows()
+    return canny
